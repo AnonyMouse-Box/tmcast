1ea37e456e6ead6768b533cef0e24511625de328
C.Monkey <36849880+AnonyMouse-Box@users.noreply.github.com>
2021-11-15 22:59:12 +0000

    Update tmcast.h

--- src/tmcast.h
+++ src/tmcast.h
@@ -1,9 +1,8 @@
 // Definitions
-#define MAX ( 10000 )                     // Set upper bound
-#define LEN(a) ( sizeof(a) / sizeof(*a) ) // Define array length
+#define MAX 10000                       // Set upper bound
+#define LEN(a) (sizeof(a) / sizeof(*a)) // Define array length
 
 // Included libraries
 
 
 // Function prototypes
-
