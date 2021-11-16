f784b692e4fd9c49abc75ab5a31cca215472de0a
C.Monkey <36849880+AnonyMouse-Box@users.noreply.github.com>
2021-11-16 17:58:08 +0000

    Update flags.h

--- src/header/flags.h
+++ src/header/flags.h
@@ -1,1 +1,13 @@
+// Definitions
+#define MAX 10000                       // Set upper bound
+#define NUM_FLAGS 1                     // Set number of flags
+#define LEN(a) (sizeof(a) / sizeof(*a)) // Define array length
+
+// Included libraries
+#include <stdio.h>
+#include <stdbool.h>
+
+// Local files
+
+// Function prototypes
 bool build_table()
