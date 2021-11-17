72b867c70680690e96e468b1844644dbdc264085
C.Monkey <36849880+AnonyMouse-Box@users.noreply.github.com>
2021-11-17 15:06:04 +0000

    Update tmcast.h

--- src/header/tmcast.h
+++ src/header/tmcast.h
@@ -1,6 +1,5 @@
 // Definitions
 #define MAX 10000                       // Set upper bound
-#define NUM_FLAGS 1                     // Set number of flags
 #define LEN(a) (sizeof(a) / sizeof(*a)) // Define array length
 
 // Included libraries
