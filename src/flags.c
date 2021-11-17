4a05976d38b6d7f9ba09acaebd81d855d652ae93
C.Monkey <36849880+AnonyMouse-Box@users.noreply.github.com>
2021-11-17 14:52:01 +0000

    Update flags.c

--- src/flags.c
+++ src/flags.c
@@ -2,39 +2,19 @@
 #include "flags.h"
 
 // Builds the hash table for the flags
-bool build_table()
+bool build_table(void)
 {
     // Set flag names
     char *names[NUM_FLAGS] = {"version"};
-    bool pointer_result = true;
+    bool node_result = true;
     int a = 0;
     do
     {
         // Generate hash of word, condense within bounds
-        unsigned int hash = superFastHash(names[a], strlen(names[a])) % UINT_MAX;
+        unsigned int hash = super_fast_hash(names[a], strlen(names[a])) % UINT_MAX;
         
-        // If no entry at hash, write flag to node
-        if (flagTable[hash] == NULL)
-        {
-            flagTable[hash] = calloc(1, sizeof(flagTupleNode));
-            
-            // Check if memory allocation was successful
-            if (flagTable[hash] != NULL)
-            {
-                pointer_result = false;
-                break;
-            }
-            
-            // Set variables for flag node tuple
-            strcpy(flagTable[hash] -> name, names[a]);
-            flagTable[hash] -> value = false;
-        }
-        
-        // If entry exists, follow the pointer to next
-        else
-        {
-            // Recursively follow linked list until null pointer is found
-        }
+        // Write new node to table
+        node_result = write_node(flagTable[hash]);
         
         // Move to next name and check if finished
         a++;
@@ -42,6 +22,35 @@ bool build_table()
         {
             return true;
         }
-    } while (pointer_result == true)
+    } while (node_result == true)
     return false;
+}
+
+bool write_node(flagTupleNode *ptrNode)
+{
+    // If no entry at hash, write flag to node
+    if (ptrNode == NULL)
+    {
+        ptrNode = calloc(1, sizeof(flagTupleNode));
+        
+        // Check if memory allocation was successful
+        if (ptrNode == NULL)
+        {
+            return false;
+        }
+        
+        // Set variables for flag node tuple
+        strcpy(ptrNode -> name, names[a]);
+        ptrNode -> value = false;
+        ptrNode -> next = NULL;
+        return true;
+    }
+    
+    // If entry exists, follow the pointer to next
+    else
+    {
+        // Recursively follow linked list until null pointer is found
+        bool ptrResult = write_node(ptrNode -> *next);
+        return ptrResult;
+    }
 }
