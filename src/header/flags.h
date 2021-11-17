76977d16351d077b036382a112f2ed8fb1b41c3b
C.Monkey <36849880+AnonyMouse-Box@users.noreply.github.com>
2021-11-17 15:01:48 +0000

    Update flags.h

--- src/header/flags.h
+++ src/header/flags.h
@@ -1,10 +1,11 @@
-// Definitions
-#define NUM_FLAGS 1                     // Set number of flags
-
 // Included libraries
 #include <stdbool.h>
 
 // Local files
 
+// Definitions
+#define NUM_FLAGS 1                     // Set number of flags
+
 // Function prototypes
-bool build_table()
+bool build_table(void);
+bool write_node(flagTupleNode *ptrNode);
